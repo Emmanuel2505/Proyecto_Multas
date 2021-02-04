d0a9cb259ebea10023f8801600d007ba6a6bac77
Eutimio <75862425+EutimioArevalo@users.noreply.github.com>
2021-02-04 17:02:48 -0500

    04/02/2021 17:02 Eutimio Arevalo Codificacion de la Registro de multas

---
+++ src/Modelo/Persona.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Persona {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+    private int Rol;
+
+    public Persona() {
+    }
+
+    public Persona(String Cedula, String nombre, String apellido, String direccion, String telefono, int Rol) {
+        this.cedula = Cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.Rol = Rol;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String Cedula) {
+        this.cedula = Cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getRol() {
+        return Rol;
+    }
+
+    public void setRol(int Rol) {
+        this.Rol = Rol;
+    }
+    
+    
+}
---
+++ src/Vista/componentes/Componentes.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista.componentes;
+
+import Controlador.DAO.MarcaDAO;
+import Controlador.DAO.NormativaDAO;
+import Controlador.DAO.TipoLicenciaDAO;
+import Controlador.ListaSimple;
+import Controlador.Utilidades;
+import Modelo.Normativa;
+import Modelo.Marca;
+import Modelo.TipoLicencia;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author timoa
+ */
+public class Componentes extends JFrame {
+
+    //JButton consultar, biblioteca, registrarM, registrarA, generarI;
+    
+    public static void cargarCombo(JComboBox cbx, ListaSimple lista, String atributoClase) {
+        cbx.removeAllItems();
+        int tamanio = lista.tamanio();
+        cbx.addItem("");
+        for (int i = 0; i < tamanio; i++) {
+                String dato = Utilidades.extraccionDato(lista.obtenerPorPosicion(i), atributoClase);
+                cbx.addItem(dato);
+        }
+    }
+    
+    public static void cargarCombo(JComboBox cbx, ListaSimple lista, String atributoClase, String mensaje) {
+        cbx.removeAllItems();
+        int tamanio = lista.tamanio();
+        cbx.addItem(mensaje);
+        for (int i = 0; i < tamanio; i++) {
+                String dato = Utilidades.extraccionDato(lista.obtenerPorPosicion(i), atributoClase);
+                cbx.addItem(dato);
+        }
+    }
+}
