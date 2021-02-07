bb8c3a93bc66bcfcbbdf0b8f09c511c0e8c1fa20
ASUS <ASUS@LAPTOP-JNGNO16T>
2021-02-06 22:02:14 -0500

    02/06/2021 22:02 Roy Leon Implementacion de obtener una lista de Personas en PersonaDAO

--- src/Controlador/DAO/PersonaDAO.java
+++ src/Controlador/DAO/PersonaDAO.java
@@ -5,6 +5,7 @@
  */
 package Controlador.DAO;
 
+import Controlador.ListaSimple;
 import Modelo.Persona;
 import javax.swing.JOptionPane;
 
@@ -63,5 +64,16 @@ public class PersonaDAO extends AdaptadorDAO{
             }
         }
         return dato;
+    }
+    
+    public ListaSimple obtenerListaPersona(long idRol){
+        ListaSimple lista = new ListaSimple();
+        for (int i = 0; i < listar().tamanio(); i++) {
+            Persona aux = (Persona)listar().obtenerPorPosicion(i);
+            if (aux.getIdRol()== idRol) {
+                lista.insertar(aux);
+            }
+        }
+        return lista;
     }
 }
